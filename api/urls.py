be632106e96746d015d36679cab66c67511d4701
Allison Alves da Silva <54888230854@>
2024-10-24 14:53:01 -0300

    first commit

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('produtos', views.listar_produtos),
+    path('listar', views.ProdutosViews.as_view()),
+    path('produto/<int:pk>/', views.ProdutosDetailViews.as_view(),)
+]
